40908c75818d7ebdc95da2be4f87dac0c13c0d18
unknown <CETECOM@AOAWSB02LC0411.duoc.acad>
2024-04-29 22:25:01 -0400

    nueva prueba :3

--- saaado.py
+++ saaado.py
@@ -1,7 +1,6 @@
 import time, os
-
-
-
+user = "admin"
+password = "admin"
 while True:
     os.system("cls")
     print("\t\tsistema de gestion de alumnos")
@@ -19,10 +18,42 @@ while True:
             direccion = input("ingrese su direccion\n")
             while direccion == "":
                 direccion = input("ingrese su direccion, no acepta cadena vacia u.u \n")
+            while rut <= 5000000 or rut >= 39999999:
+                rut = int(input(" ingrese su rut, >= a 5M y =< 39.9M \n"))
             edad = int(input("ingrese edad \n"))
+            while edad < 17 or edad >90:
+                edad = int(input("ingrese su edad  \n"))
+            correo = input("ingrese su correo\n")
+            while '@' not in correo:
+                correo = input("ingrese su correo\n")
+            nem = float(input("ingrese nem\n"))
+            
+            
         elif opcion == 2:
+            os.system("cls")
             print("CONSULTA DATOS ALUMNO")
-            x = input()
-        
+            username = input("ingrese usuario \n")
+            psswrd = input("ingrese password\n")
+            if(user == username and password == psswrd):
+                rut_alumno = int(input("ingrese rut de alumno a buscar \n"))
+                if rut_alumno == rut:
+                    print("NOMBRE: ", nombre)
+                    print("Direccion: ", direccion)
+                    print(f"Edad: {edad}")
+                    print("Correo: ", correo)
+                    print(f"Nem: {nem}")
+                    if nem <= 520:
+                        print("CAGASTE")
+                    else:
+                        print("pasastes :D")
+                        
+                else:
+                    print("el rut ingresado no existe")
+                print("pulse tecla para continuar uwu ")
+                x = input()
+            elif opcion == 3:
+                os.system("cls")
+                print("SALIR")
+            print("Ha salido del sistema")
     except:
         print("opcion no existe pooo ")
